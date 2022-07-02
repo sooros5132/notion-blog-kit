2e594951253469072f1d49905e3a6795ab9dbe90
sooros5132 <sooros5132@gmail.com>
2022-07-02 15:00:30 +0900

    aws 이미지 링크를 notion 으로 변경

--- src/components/modules/NotionRender.tsx
+++ src/components/modules/NotionRender.tsx
@@ -255,9 +255,12 @@ const NotionRender: React.FC<NotionRenderProps> = ({ slug }): JSX.Element => {
           <PageInfoCover>
             <DatabaseItemCoverImage>
               <Image
-                src={pages?.cover?.[pages?.cover?.type]?.url!}
+                src={convertAwsImageObjectUrlToNotionUrl({
+                  blockId: pages.id,
+                  s3ObjectUrl: pages?.cover?.[pages?.cover?.type]?.url!
+                })}
                 placeholder='blur'
-                blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=='
+                blurDataURL='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
                 layout='fill'
                 objectFit='cover'
               />
@@ -553,9 +556,12 @@ const ChildDatabase: React.FC<ChildDatabaseProps> = ({ block, databases }) => {
                   {database?.cover && (
                     <DatabaseItemCoverImage>
                       <Image
-                        src={database.cover.file.url}
+                        src={convertAwsImageObjectUrlToNotionUrl({
+                          blockId: database.id,
+                          s3ObjectUrl: database?.cover?.file?.url
+                        })}
                         placeholder='blur'
-                        blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=='
+                        blurDataURL='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
                         layout='fill'
                         objectFit='cover'
                       />
@@ -576,6 +582,44 @@ const ChildDatabase: React.FC<ChildDatabaseProps> = ({ block, databases }) => {
     </div>
   );
 };
+
+function convertAwsImageObjectUrlToNotionUrl({
+  blockId,
+  s3ObjectUrl,
+  table = 'block'
+}: {
+  s3ObjectUrl: string;
+  blockId: string;
+  table?: string;
+}) {
+  // s3ObjectUrl: https://s3.us-west-2.amazonaws.com/secure.notion-static.com/8f7f9f31-56f7-49c3-a05f-d15ac4a722ca/qemu.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220702%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220702T053925Z&X-Amz-Expires=3600&X-Amz-Signature=050701d9bc05ec877366b066584240a31a4b5d2459fe6b7f39243e90d479addd&X-Amz-SignedHeaders=host&x-id=GetObject
+  // pageId: 12345678-abcd-1234-abcd-123456789012
+  try {
+    if (!table || !blockId || !s3ObjectUrl) {
+      return s3ObjectUrl;
+    }
+    const s3Url = new URL(s3ObjectUrl);
+
+    if (
+      !s3Url?.origin?.includes('amazonaws.com') ||
+      !s3Url?.pathname?.includes('secure.notion-static.com')
+    ) {
+      return s3ObjectUrl;
+    }
+
+    const s3FileUuid = s3Url.pathname.replace(/^\/secure\.notion\-static\.com\//, '');
+
+    if (!s3FileUuid) {
+      return s3ObjectUrl;
+    }
+
+    return `https://www.notion.so/image/${encodeURIComponent(
+      'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/' + s3FileUuid
+    )}?table=${table}&id=${blockId}`;
+  } catch (e) {
+    return s3ObjectUrl;
+  }
+}
 
 NotionRender.displayName = 'NotionRender';
 
