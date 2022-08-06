535daf2b9b90e1046736279d62d5baf523af21bd
SOOROS <sooros5132@gmail.com>
2022-08-06 23:50:32 +0900

    toggle을 html 기본 태그 기능으로 변경

--- src/components/modules/NotionRender.tsx
+++ src/components/modules/NotionRender.tsx
@@ -964,28 +964,16 @@ interface ToggleProps {
 }
 
 const Toggle: React.FC<ToggleProps> = ({ block, blocks, chilrenBlockDepth }) => {
-  const [isOpen, setOpen] = useState(false);
-  const handleClickToggleButton = () => {
-    setOpen((prev) => !prev);
-  };
   return (
     <div className={block.toggle.color !== 'default' ? notionColor[block.toggle.color] : undefined}>
-      <NotionBlockRender
-        block={block}
-        blocks={blocks}
-        chilrenBlockDepth={isOpen ? chilrenBlockDepth : undefined}
-      >
-        <div className='cursor-pointer'>
-          <div className='flex items-center' onClick={handleClickToggleButton}>
-            <div className={`${isOpen ? 'rotate-90' : ''}`}>
-              <TiChevronRight />
-            </div>
-            <div className='flex-auto'>
-              <Paragraph blockId={block.id} richText={block.toggle.rich_text} />
-            </div>
+      <details className='notion-toggle'>
+        <summary className='flex cursor-pointer marker'>
+          <div className='flex-auto'>
+            <Paragraph blockId={block.id} richText={block.toggle.rich_text} />
           </div>
-        </div>
-      </NotionBlockRender>
+        </summary>
+        <NotionBlockRender block={block} blocks={blocks} chilrenBlockDepth={chilrenBlockDepth} />
+      </details>
     </div>
   );
 };
