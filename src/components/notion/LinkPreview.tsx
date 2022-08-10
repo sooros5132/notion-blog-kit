c26212fa4310d656e21612c256c32fb9085ca331
SOOROS <sooros5132@gmail.com>
2022-08-11 01:36:59 +0900

    LinkPreview 이미지 여백 생기는 오류 수정

--- src/components/notion/LinkPreview.tsx
+++ src/components/notion/LinkPreview.tsx
@@ -30,7 +30,7 @@ const LinkPreview: React.FC<LinkPreviewProps> = ({ url }) => {
 
   return (
     <a href={relativePath} rel='noreferrer' target='_blank'>
-      <div className='flex-col-reverse rounded-sm shadow-xl card card-side bg-base-100 sm:flex-row'>
+      <div className='flex-col-reverse rounded-sm shadow-xl card card-side bg-base-100 sm:flex-row sm:min-h-[150px] sm:max-h-[250px]'>
         <div className='px-4 py-3 card-body basis-full sm:basis-[60%] md:basis-[65%]'>
           <h2 className='text-lg card-title line-clamp-2'>
             <NotionParagraphText>{data?.title}</NotionParagraphText>
@@ -40,7 +40,7 @@ const LinkPreview: React.FC<LinkPreviewProps> = ({ url }) => {
           </p>
           <div className='mt-auto text-sm'>
             <div className='flex-grow-0 break-all gap-x-1 text-ellipsis'>
-              <p>
+              <p className='line-clamp-2'>
                 {data?.icon && (
                   <img
                     className='inline-block w-[1.1em] h-[1.1em] align-text-bottom mr-1'
@@ -55,7 +55,7 @@ const LinkPreview: React.FC<LinkPreviewProps> = ({ url }) => {
         {data?.image?.url && (
           <figure className='image-wrapper shrink-0 basis-full sm:min-h-full sm:basis-[40%] md:basis-[35%]'>
             <img
-              className='w-full sm:h-full min-h-[200px] sm:min-h-[125px] max-h-[175px]'
+              className='w-full sm:h-full min-h-[200px] max-h-[200px] sm:min-h-[125px] sm:max-h-[initial]'
               src={
                 data.image.url.charAt(0) === '/'
                   ? new URL(data.image.url, url).href
