7ef9b4b328ae740ac8a7d5706e2805e35d8cd3cc
SOOROS <sooros5132@gmail.com>
2023-01-10 04:09:28 +0900

    footer 글씨 크기 축소

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,8 +7,8 @@ const Footer: React.FC = (): JSX.Element => {
   return (
     <footer className='px-2 py-4 sm:px-4 bg-primary text-primary-content'>
       <div className='max-w-screen-lg mx-auto flex justify-between align-center'>
-        <div className='text-3xl font-bold'>{config.infomation.blogname}</div>
-        <div className='flex items-center gap-2 text-3xl'>
+        <div className='text-2xl font-bold'>{config.infomation.blogname}</div>
+        <div className='flex items-center gap-2 text-2xl'>
           {config.infomation.email && (
             <a href={'mailto:' + config.infomation.email}>
               <SiMaildotru />
