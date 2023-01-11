b5157887b2ad9d104c5e7e990676b879926630c8
SOOROS <sooros5132@gmail.com>
2023-01-11 22:30:44 +0900

    youtube embed 에러 수정

--- src/components/notion/lib/Video.tsx
+++ src/components/notion/lib/Video.tsx
@@ -6,6 +6,7 @@ import { fetcher } from 'src/lib/swr';
 import type { NotionBlock } from 'src/types/notion';
 import useSWR from 'swr';
 import { NotionParagraphBlock } from '.';
+import queryString from 'querystring';
 
 interface VideoProps {
   block: NotionBlock;
@@ -37,51 +38,56 @@ const VideoBlockInner: React.FC<VideoProps> = ({ block }) => {
   );
   if (error) {
     return (
-      <div className='w-full'>
-        <div className='flex-center py-0.5 bg-gray-900'>
-          <div className='flex items-center text-notionColor-red'>
-            <IoClose />
-          </div>
-          &nbsp;
-          <p>비디오 정보를 불러올 수 없습니다.</p>
+      <div className='flex-center py-0.5 bg-gray-900'>
+        <div className='flex items-center text-notionColor-red'>
+          <IoClose />
         </div>
+        &nbsp;
+        <p>비디오 정보를 불러올 수 없습니다.</p>
       </div>
     );
   }
 
   if (isValidating || (!data?.video?.file?.url && !data?.video?.external?.url)) {
     return (
-      <div className='w-full'>
-        <div className='flex-center h-[50vw] max-h-[20rem] bg-notionColor-gray'>
-          <p>비디오 정보를 불러오고 있습니다.</p>
-        </div>
+      <div className='flex-center h-[50vw] max-h-[20rem] bg-notionColor-gray'>
+        <p>비디오 정보를 불러오고 있습니다.</p>
       </div>
     );
   }
-
-  return (
-    <div className='w-full'>
-      {data?.video.type === 'file' ? (
-        <video className='w-full aspect-video' controls src={data?.video.file?.url} />
-      ) : (
-        <div className='w-full [&>iframe]:w-full [&>iframe]:aspect-video '>
-          <EmbedVideo url={data?.video.external?.url || ''} />
-        </div>
-      )}
-    </div>
-  );
+  if (data?.video.type === 'file') {
+    return <video className='w-full aspect-video' controls src={data?.video.file?.url} />;
+  } else {
+    return (
+      <div className='w-full [&>iframe]:w-full [&>iframe]:aspect-video '>
+        <EmbedVideo url={data?.video.external?.url || ''} />
+      </div>
+    );
+  }
 };
 
 const EmbedVideo: React.FC<{ url: string }> = ({ url }) => {
-  const hostname = new URL(url).hostname;
+  const originalURL = new URL(url);
 
-  switch (hostname) {
+  switch (originalURL.hostname) {
     case 'youtube.com':
     case 'www.youtube.com': {
-      const embedUrl = url.replace(/\/watch\?v=/, '/embed/');
+      // const embedUrl = url.replace(/\/watch\?v=/, '/embed/');
+      let embedUrl = '';
+      const urlSearch =
+        originalURL.search.charAt(0) === '?' ? originalURL.search.slice(1) : originalURL.search;
+      const searchParams = queryString.decode(urlSearch);
+
+      if (originalURL.pathname === '/watch' && typeof searchParams['v'] === 'string') {
+        const { v: _, ...newSearchParams } = searchParams;
+        embedUrl = `https://www.youtube.com/embed/${searchParams['v']}?${queryString.encode(
+          newSearchParams
+        )}`;
+      }
+
       return (
         <iframe
-          src={embedUrl}
+          src={embedUrl || url}
           allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share'
           allowFullScreen
         />
