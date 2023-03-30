de2b0a635845c7e6cce4a1cc5848d10b1006c983
SOOROS <sooros5132@gmail.com>
2023-03-30 15:10:44 +0900

    seo 이미지들 고정 너비 추가

--- src/components/notion/lib/NotionSeo.tsx
+++ src/components/notion/lib/NotionSeo.tsx
@@ -19,9 +19,16 @@ const NotionSeo: React.FC<NotionSeoProps> = ({ page, title, description, slug })
       ? awsImageObjectUrlToNotionUrl({
           blockId: page.id,
           s3ObjectUrl: page.cover.file?.url || ''
-        })
+        }) + '&width=1200'
       : ''
     : '';
+  const icon =
+    page.icon?.file &&
+    page.icon?.type === 'file' &&
+    awsImageObjectUrlToNotionUrl({
+      blockId: page.id,
+      s3ObjectUrl: page.icon.file.url
+    });
 
   return (
     <>
@@ -43,28 +50,16 @@ const NotionSeo: React.FC<NotionSeoProps> = ({ page, title, description, slug })
         }}
       />
       <Head>
-        {page.icon?.file && page.icon?.type === 'file' && (
-          <link
-            rel='apple-touch-icon'
-            href={awsImageObjectUrlToNotionUrl({
-              blockId: page.id,
-              s3ObjectUrl: page.icon.file.url
-            })}
-          />
+        {icon && (
+          <link rel='apple-touch-icon' href={icon.includes('?') ? icon + '&width=192' : icon} />
         )}
-        {page.icon?.file && page.icon?.type === 'file' && (
-          <link
-            rel='shortcut icon'
-            href={awsImageObjectUrlToNotionUrl({
-              blockId: page.id,
-              s3ObjectUrl: page.icon.file.url
-            })}
-          />
+        {icon && (
+          <link rel='shortcut icon' href={icon.includes('?') ? icon + '&width=256' : icon} />
         )}
         {page.icon?.emoji && page.icon?.type === 'emoji' && (
           <link
             rel='shortcut icon'
-            href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${page.icon?.emoji}</text></svg>`}
+            href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${page.icon.emoji}</text></svg>`}
           />
         )}
       </Head>
