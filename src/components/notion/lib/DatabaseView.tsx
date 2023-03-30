601e87486ed4abf07b079be995655118fedaf3b9
SOOROS <sooros5132@gmail.com>
2023-03-30 13:44:53 +0900

    category 렌더 추가

--- src/components/notion/lib/DatabaseView.tsx
+++ src/components/notion/lib/DatabaseView.tsx
@@ -76,7 +76,7 @@ export const NotionDatabasePageView: React.FC<NotionDatabasePageViewProps> = ({
 
   const categorys = useMemo(() => {
     if (pageInfo.properties.category?.type !== 'select') {
-      return null;
+      return {};
     }
     const categorys = baseBlock.results.reduce<Record<string, number>>((prev, current) => {
       const name = current.properties.category?.select.name;
@@ -91,28 +91,28 @@ export const NotionDatabasePageView: React.FC<NotionDatabasePageViewProps> = ({
     return categorys;
   }, [baseBlock.results, pageInfo.properties.category?.type]);
 
+  const categoryKeys = Object.keys(categorys).sort();
+
   return (
     <div className='w-full flex flex-col gap-y-4 sm:gap-x-4 sm:flex-row min-h-[50vh]'>
       {categorys && (
-        <aside className='sticky top-[var(--header-height)] left-0 p-2 gap-x-2 z-10 bg-base-100 flex grow-0 shrink-0 sm:z-0 sm:p-0 sm:mb-0 sm:flex-col sm:w-[200px] sm:gap-y-4 md:w-[220px]'>
-          <ul className='flex gap-x-2 overflow-x-auto scrollbar-hidden text-sm sm:flex-col sm:px-2 sm:overflow-hidden sm:gap-y-1 sm:order-2'>
-            {Object.keys(categorys)
-              .sort()
-              .map((category) => (
-                <li
-                  key={category}
-                  className={classNames(
-                    'flex items-center cursor-pointer py-[0.2em] px-[0.7em] rounded-full bg-base-content/10 sm:p-0 sm:bg-[initial] sm:rounded-none sm:hover:bg-base-content/5 sm:border-l-[3px] sm:pl-2 sm:py-0.5 border-base-content/10',
-                    filterKey === category ? 'bg-success/50 sm:font-bold sm:border-success' : null
-                  )}
-                  onClick={handleClickCategoryItem(category)}
-                >
-                  <span className='flex-auto grow-0 overflow-hidden text-ellipsis'>{category}</span>
-                  <span className='flex-auto grow-0 shrink-0'>({categorys[category]})</span>
-                </li>
-              ))}
+        <aside className='sticky top-[var(--header-height)] left-0 flex grow-0 shrink-0 p-2 gap-x-2 z-10 bg-base-100 text-sm sm:z-0 sm:p-0 sm:mb-0 sm:flex-col sm:w-[200px] sm:gap-y-4 md:w-[220px]'>
+          <ul className='flex grow shrink gap-x-2 overflow-x-auto scrollbar-hidden whitespace-nowrap sm:flex-col sm:px-2 sm:overflow-hidden sm:gap-y-1 sm:order-2'>
+            {categoryKeys.map((category) => (
+              <li
+                key={category}
+                className={classNames(
+                  'flex items-center cursor-pointer py-[0.2em] px-[0.7em] rounded-full bg-base-content/10 sm:p-0 sm:bg-[initial] sm:rounded-none sm:hover:bg-base-content/5 sm:border-l-[3px] sm:pl-2 sm:pr-1 sm:py-0.5 border-base-content/10',
+                  filterKey === category ? 'bg-success/50 sm:font-bold sm:border-success' : null
+                )}
+                onClick={handleClickCategoryItem(category)}
+              >
+                <span className='flex-auto grow-0 overflow-hidden text-ellipsis'>{category}</span>
+                <span className='flex-auto grow-0 shrink-0'>({categorys[category]})</span>
+              </li>
+            ))}
           </ul>
-          <div className='self-center input-group min-w-[180px] bg-base-100 rounded-md shadow-md dark:bg-base-content/5 sm:order-1'>
+          <div className='self-center flex-[0] shrink-0 input-group min-w-[180px] bg-base-100 rounded-md shadow-md dark:bg-base-content/5 sm:order-1'>
             <input
               className={classNames(
                 'input input-sm w-full bg-transparent focus:outline-none placeholder:text-base-content/60'
@@ -146,9 +146,10 @@ type ArticleSummaryProps = {
 
 const ArticleSummary: React.FC<ArticleSummaryProps> = ({ article }) => {
   const { id, created_time, properties, icon, cover } = article;
-  const { tags, title: titleProperty } = properties;
+  const { category: categoryProperty, tags, title: titleProperty } = properties;
   const haveTagProperty = tags?.type === 'multi_select';
 
+  const category = categoryProperty?.select ? categoryProperty.select.name : null;
   const title = titleProperty?.title
     ? titleProperty.title.map((text) => text?.plain_text).join('') || null
     : null;
@@ -163,10 +164,10 @@ const ArticleSummary: React.FC<ArticleSummaryProps> = ({ article }) => {
             )}`
           : `/${id.replaceAll('-', '')}`
       }
-      className='[&_.cover-image]:hover:brightness-110 [&_.cover-image>div]:transition-transform [&_.cover-image>div]:duration-[400ms] [&_.cover-image>div]:hover:scale-[1.2]'
+      className='[&_.cover-image]:hover:brightness-110 [&_.cover-image>div]:transition-transform [&_.cover-image>div]:duration-[400ms] [&_.cover-image>div]:hover:scale-[1.1]'
     >
       <div className='w-full flex flex-col bg-base-content/5 shadow-md overflow-hidden rounded-md isolate sm:flex-row'>
-        <div className='cover-image shrink-0 h-[200px] bg-base-content/5 overflow-hidden brightness-95 transition-[filter] ease-linear duration-300 [&>div]:h-full [&>div>img]:w-full [&>div>img]:h-full sm:w-[100px] md:w-[150px] lg:w-[200px] sm:h-[84px]'>
+        <div className='cover-image shrink-0 h-[200px] bg-base-content/5 overflow-hidden brightness-95 transition-[filter] ease-linear duration-300 [&>div]:h-full [&>div>img]:w-full [&>div>img]:h-full sm:w-[100px] md:w-[150px] lg:w-[200px] sm:h-[100px]'>
           {cover ? (
             <NotionSecureImage
               blockId={id}
@@ -199,7 +200,10 @@ const ArticleSummary: React.FC<ArticleSummaryProps> = ({ article }) => {
         </div>
         <div className='flex-auto flex flex-col justify-between p-4 py-3 sm:py-2 break-all'>
           <div className='line-clamp-2'>{title}</div>
-          <div className='mt-auto flex items-end justify-between gap-x-2 text-sm'>
+          {category && (
+            <div className='mt-auto text-xs text-zinc-500 line-clamp-1 text-right'>{category}</div>
+          )}
+          <div className='flex items-end justify-between gap-x-2 text-sm'>
             <div className='flex-1 line-clamp-1'>
               {haveTagProperty &&
                 tags.multi_select?.map((tag, idx) => (
